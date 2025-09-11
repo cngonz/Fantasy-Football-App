54fb53bbc278b3312e8d87a5719b380d3832063a
cngonz <cn.gonzalez@ufl.edu>
2025-09-11 15:10:59 -0400

    Initial Commit

---
+++ FantasyApp/FantasyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FantasyAppApp.swift
+//  FantasyApp
+//
+//  Created by Cesar N. Gonzalez on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FantasyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FantasyAppTests/FantasyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FantasyAppTests.swift
+//  FantasyAppTests
+//
+//  Created by Cesar N. Gonzalez on 9/11/25.
+//
+
+import Testing
+@testable import FantasyApp
+
+struct FantasyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
